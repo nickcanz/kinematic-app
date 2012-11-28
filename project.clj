7ee0ae87c3e5ac4961d5907cfa6b78e66b031a27
Josh <kburton@relaynetwork.com>
2012-11-27 21:00:01 -0500

    releasing first non-snapshot version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject kinematic-app/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject kinematic-app/lein-template "0.1.0"
+  :description "A lein template to generate a basic Kinematic app"
+  :url "http://github.com/jcrean/kinematic-app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
