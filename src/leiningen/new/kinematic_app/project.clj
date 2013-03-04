b7d7bd0e35fbe013155bb05ba1e6aeb365804b86
nickcanz <canzoneri.nicholas@gmail.com>
2013-03-03 20:27:39 -0500

    bumping deps to use clojure 1.5

--- src/leiningen/new/kinematic_app/project.clj
+++ src/leiningen/new/kinematic_app/project.clj
@@ -1,9 +1,7 @@
 (defproject {{name}} "1.0.0-SNAPSHOT"
   :description "FIXME"
-  :dev-dependencies [[swank-clojure "1.4.2"]]
-  :dependencies [[org.clojure/clojure         "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [swank-clojure               "1.4.2"]
-                 [ring                        "1.1.0"]
-                 [com.relaynetwork/kinematic  "1.2.3"]
-                 [com.relaynetwork/clorine    "1.0.12"]])
+  :plugins [[lein-swank "1.4.5"]]
+  :dependencies [[org.clojure/clojure         "1.5.0"]
+                 [ring                        "1.1.8"]
+                 [swank-clojure               "1.4.5"]
+                 [com.relaynetwork/kinematic  "1.3.0"]])
