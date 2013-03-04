3b3fbff3428d8b98b2802baeb6653f7b2bb8eb20
nickcanz <canzoneri.nicholas@gmail.com>
2013-03-03 20:29:54 -0500

    do not use non-existant version of swank clojure

--- src/leiningen/new/kinematic_app/project.clj
+++ src/leiningen/new/kinematic_app/project.clj
@@ -3,5 +3,5 @@
   :plugins [[lein-swank "1.4.5"]]
   :dependencies [[org.clojure/clojure         "1.5.0"]
                  [ring                        "1.1.8"]
-                 [swank-clojure               "1.4.5"]
+                 [swank-clojure               "1.4.3"]
                  [com.relaynetwork/kinematic  "1.3.0"]])
